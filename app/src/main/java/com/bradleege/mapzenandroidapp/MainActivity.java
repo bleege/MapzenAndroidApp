1edc6bc17b5c2342a992e5f954d794b769316a8c
Brad Leege <bleege@gmail.com>
2016-12-27 17:25:30 -0600

    Basic Map Setup

--- app/src/main/java/com/bradleege/mapzenandroidapp/MainActivity.java
+++ app/src/main/java/com/bradleege/mapzenandroidapp/MainActivity.java
@@ -3,11 +3,23 @@ package com.bradleege.mapzenandroidapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapzen.android.graphics.MapFragment;
+import com.mapzen.android.graphics.MapzenMap;
+import com.mapzen.android.graphics.OnMapReadyCallback;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override public void onMapReady(MapzenMap map) {
+                // Map is ready.
+            }
+        });
+
     }
 }
